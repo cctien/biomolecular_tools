fc78b58104147599905f84cdbd864ebb79586b5e
Chih-chan Tien <chihchan.tien@gmail.com>
2025-08-25 18:18:12 -0500

    updated

---
+++ install.md
@@ -0,0 +1,8 @@
+# install
+
+```sh
+conda create -n molecular_modelling_tools python=3.13
+conda activate molecular_modelling_tools
+pip install -r requirements_python/requirements.txt
+
+```
---
+++ src/molecular_modelling_tools/__init__.py
@@ -0,0 +1,9 @@
+# ....................{ BIG BEAR                        }....................
+# Warn about type hint violations in *OTHER* packages outside your control;
+# only raise exceptions from violations in your package under your control.
+# Again, at the very top of your "{your_package}.__init__" submodule:
+from beartype import BeartypeConf  # <-- this isn't your fault
+from beartype.claw import beartype_all, beartype_this_package  # <-- you didn't sign up for this
+
+beartype_this_package()  # <-- raise exceptions in your code
+beartype_all(conf=BeartypeConf(violation_type=UserWarning))  # <-- emit warnings from other code

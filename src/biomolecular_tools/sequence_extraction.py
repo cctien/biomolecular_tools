d852e7f296edf28f86c227051c77f0a0bd99e939
Chih-chan Tien <chihchan.tien@gmail.com>
2025-08-25 21:51:58 -0500

    updated

--- src/biomolecular_tools/sequence_extraction.py
+++ src/biomolecular_tools/sequence_extraction.py
@@ -1,30 +1,11 @@
 import logging
 from collections.abc import Mapping
 
-import biotite.structure
-import biotite.structure as struc
 import biotite.structure.io
 from biotite.sequence import ProteinSequence
 from biotite.structure import AtomArray, filter_amino_acids, get_chains, get_residue_starts
 
 logger = logging.getLogger(__name__)
-
-
-def extract_sequences(pdb_file: str) -> Mapping[str, str]:
-    # Load structure from PDB file
-    structure = biotite.structure.io.load_structure(pdb_file)
-    structure = structure[filter_amino_acids(structure)]
-    sequences: Mapping[str, str] = {}
-    for chain_id in get_chains(structure):
-        chain = structure[structure.chain_id == chain_id]
-        _sequence = get_residue_starts(chain, add_exclusive_stop=False)
-        res_names = tuple(chain.res_name[_sequence].tolist())
-        sequence = ProteinSequence(res_names)
-        assert sequence.is_valid(), f"Invalid sequence for chain {chain_id} in {pdb_file}"
-        sequences[chain_id.item()] = "".join(sequence.symbols.tolist())
-
-    logger.debug(f"Extracted sequences from {pdb_file}: {sequences}")
-    return sequences
 
 
 def protein_sequences_ex_structure_file(filepath: str) -> Mapping[str, ProteinSequence]:
@@ -39,6 +20,13 @@ def protein_sequences_ex_structure_file(filepath: str) -> Mapping[str, ProteinSe
         assert sequence.is_valid(), f"Invalid sequence for chain {chain_id} in {filepath}"
         sequences[chain_id.item()] = sequence
 
+    return sequences
+
+
+def extract_sequences(pdb_file: str) -> Mapping[str, str]:
+    _sequences = protein_sequences_ex_structure_file(pdb_file)
+    sequences = {c: "".join(s.symbols.tolist()) for c, s in _sequences.items()}
+    logger.debug(f"Extracted sequences from {pdb_file}: {sequences}")
     return sequences
 
 

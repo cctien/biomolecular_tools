e364b1e45b6ef63ef0195a16095cbc7a5a037af6
Chih-chan Tien <chihchan.tien@gmail.com>
2025-09-09 08:52:24 -0500

    frozenbidict

--- src/biomolecular_tools/amino_acids.py
+++ src/biomolecular_tools/amino_acids.py
@@ -1,6 +1,6 @@
-from biotite.sequence import ProteinSequence
+from bidict import frozenbidict
 
-_dict_1to3 = {
+_amino_acid_1to3_unambiguous = {
     "A": "ALA",  # Alanine
     "C": "CYS",  # Cysteine
     "D": "ASP",  # Aspartic acid
@@ -21,84 +21,81 @@ _dict_1to3 = {
     "V": "VAL",  # Valine
     "W": "TRP",  # Tryptophan
     "Y": "TYR",  # Tyrosine
-    "O": "PYL",  # Pyrrolysine (non-standard amino acid)
-    "U": "SEC",  # Selenocysteine (non-standard amino acid)
+    "O": "PYL",  # Pyrrolysine (the 22nd proteinogenic amino acid)
+    "U": "SEC",  # Selenocysteine (the 21st proteinogenic amino acid)
+}
+_amino_acid_1to3_ambiguous = {
+    **_amino_acid_1to3_unambiguous,
+    "X": "XAA",  # Any amino acid (ambiguous code)
     # "B": "ASX",  # Aspartic acid or Asparagine (ambiguous code)
     # "J": "XLE",  # Leucine or Isoleucine (ambiguous code)
-    # "X": "XAA",  # Any amino acid (ambiguous code)
     # "Z": "GLX",  # Glutamic acid or Glutamine (ambiguous code)
 }
-_dict_3to1 = {v: k for k, v in _dict_1to3.items()}
+amino_acid_code_1to3_unambiguous = frozenbidict(_amino_acid_1to3_unambiguous)
+amino_acid_code_1to3 = frozenbidict(_amino_acid_1to3_ambiguous)
 
 
-def amino_acid_code_one_to_three_strict(one_letter_code: str) -> str:
-    return _dict_1to3[one_letter_code.upper()]
+def amino_acid_code_1_to_3(one_letter_code: str) -> str | None:
+    return amino_acid_code_1to3.get(one_letter_code.upper())
 
 
-def amino_acid_code_three_to_one_strict(three_letter_code: str) -> str:
-    return _dict_3to1[three_letter_code.upper()]
-
-
-def amino_acid_code_one_to_three(one_letter_code: str) -> str | None:
-    return _dict_1to3.get(one_letter_code.upper())
-
-
-def amino_acid_code_three_to_one(three_letter_code: str) -> str | None:
-    return _dict_3to1.get(three_letter_code.upper())
+def amino_acid_code_3_to_1(three_letter_code: str) -> str | None:
+    return amino_acid_code_1to3.inverse.get(three_letter_code.upper())
 
 
 def _test():
-    assert amino_acid_code_one_to_three("A") == "ALA"
-    assert amino_acid_code_one_to_three("C") == "CYS"
-    assert amino_acid_code_one_to_three("D") == "ASP"
-    assert amino_acid_code_one_to_three("E") == "GLU"
-    assert amino_acid_code_one_to_three("F") == "PHE"
-    assert amino_acid_code_one_to_three("G") == "GLY"
-    assert amino_acid_code_one_to_three("H") == "HIS"
-    assert amino_acid_code_one_to_three("I") == "ILE"
-    assert amino_acid_code_one_to_three("K") == "LYS"
-    assert amino_acid_code_one_to_three("L") == "LEU"
-    assert amino_acid_code_one_to_three("M") == "MET"
-    assert amino_acid_code_one_to_three("N") == "ASN"
-    assert amino_acid_code_one_to_three("P") == "PRO"
-    assert amino_acid_code_one_to_three("Q") == "GLN"
-    assert amino_acid_code_one_to_three("R") == "ARG"
-    assert amino_acid_code_one_to_three("S") == "SER"
-    assert amino_acid_code_one_to_three("T") == "THR"
-    assert amino_acid_code_one_to_three("V") == "VAL"
-    assert amino_acid_code_one_to_three("W") == "TRP"
-    assert amino_acid_code_one_to_three("Y") == "TYR"
-    # assert amino_acid_code_one_to_three("B") == "ASX"
-    # assert amino_acid_code_one_to_three("Z") == "GLX"
-    assert amino_acid_code_one_to_three("U") == "SEC"
-    assert amino_acid_code_one_to_three("O") == "PYL"
-    assert amino_acid_code_one_to_three("X") is None
+    assert amino_acid_code_1_to_3("A") == "ALA"
+    assert amino_acid_code_1_to_3("C") == "CYS"
+    assert amino_acid_code_1_to_3("D") == "ASP"
+    assert amino_acid_code_1_to_3("E") == "GLU"
+    assert amino_acid_code_1_to_3("F") == "PHE"
+    assert amino_acid_code_1_to_3("G") == "GLY"
+    assert amino_acid_code_1_to_3("H") == "HIS"
+    assert amino_acid_code_1_to_3("I") == "ILE"
+    assert amino_acid_code_1_to_3("K") == "LYS"
+    assert amino_acid_code_1_to_3("L") == "LEU"
+    assert amino_acid_code_1_to_3("M") == "MET"
+    assert amino_acid_code_1_to_3("N") == "ASN"
+    assert amino_acid_code_1_to_3("P") == "PRO"
+    assert amino_acid_code_1_to_3("Q") == "GLN"
+    assert amino_acid_code_1_to_3("R") == "ARG"
+    assert amino_acid_code_1_to_3("S") == "SER"
+    assert amino_acid_code_1_to_3("T") == "THR"
+    assert amino_acid_code_1_to_3("V") == "VAL"
+    assert amino_acid_code_1_to_3("W") == "TRP"
+    assert amino_acid_code_1_to_3("Y") == "TYR"
+    # assert amino_acid_code_1_to_3("B") == "ASX"
+    # assert amino_acid_code_1_to_3("Z") == "GLX"
+    assert amino_acid_code_1_to_3("U") == "SEC"
+    assert amino_acid_code_1_to_3("O") == "PYL"
+    assert amino_acid_code_1_to_3("X") == "XAA"
+    assert amino_acid_code_1_to_3("Z") is None
 
-    assert amino_acid_code_three_to_one("Ala") == "A"
-    assert amino_acid_code_three_to_one("Cys") == "C"
-    assert amino_acid_code_three_to_one("Asp") == "D"
-    assert amino_acid_code_three_to_one("Glu") == "E"
-    assert amino_acid_code_three_to_one("Phe") == "F"
-    assert amino_acid_code_three_to_one("Gly") == "G"
-    assert amino_acid_code_three_to_one("His") == "H"
-    assert amino_acid_code_three_to_one("Ile") == "I"
-    assert amino_acid_code_three_to_one("Lys") == "K"
-    assert amino_acid_code_three_to_one("Leu") == "L"
-    assert amino_acid_code_three_to_one("Met") == "M"
-    assert amino_acid_code_three_to_one("Asn") == "N"
-    assert amino_acid_code_three_to_one("Pro") == "P"
-    assert amino_acid_code_three_to_one("Gln") == "Q"
-    assert amino_acid_code_three_to_one("Arg") == "R"
-    assert amino_acid_code_three_to_one("Ser") == "S"
-    assert amino_acid_code_three_to_one("Thr") == "T"
-    assert amino_acid_code_three_to_one("Val") == "V"
-    assert amino_acid_code_three_to_one("Trp") == "W"
-    assert amino_acid_code_three_to_one("Tyr") == "Y"
-    # assert amino_acid_code_three_to_one("Asx") == "B"
-    # assert amino_acid_code_three_to_one("Glx") == "Z"
-    assert amino_acid_code_three_to_one("Sec") == "U"
-    assert amino_acid_code_three_to_one("Pyl") == "O"
-    assert amino_acid_code_three_to_one("Foo") is None
+    assert amino_acid_code_3_to_1("Ala") == "A"
+    assert amino_acid_code_3_to_1("Cys") == "C"
+    assert amino_acid_code_3_to_1("Asp") == "D"
+    assert amino_acid_code_3_to_1("Glu") == "E"
+    assert amino_acid_code_3_to_1("Phe") == "F"
+    assert amino_acid_code_3_to_1("Gly") == "G"
+    assert amino_acid_code_3_to_1("His") == "H"
+    assert amino_acid_code_3_to_1("Ile") == "I"
+    assert amino_acid_code_3_to_1("Lys") == "K"
+    assert amino_acid_code_3_to_1("Leu") == "L"
+    assert amino_acid_code_3_to_1("Met") == "M"
+    assert amino_acid_code_3_to_1("Asn") == "N"
+    assert amino_acid_code_3_to_1("Pro") == "P"
+    assert amino_acid_code_3_to_1("Gln") == "Q"
+    assert amino_acid_code_3_to_1("Arg") == "R"
+    assert amino_acid_code_3_to_1("Ser") == "S"
+    assert amino_acid_code_3_to_1("Thr") == "T"
+    assert amino_acid_code_3_to_1("Val") == "V"
+    assert amino_acid_code_3_to_1("Trp") == "W"
+    assert amino_acid_code_3_to_1("Tyr") == "Y"
+    # assert amino_acid_code_3_to_1("Asx") == "B"
+    # assert amino_acid_code_3_to_1("Glx") == "Z"
+    assert amino_acid_code_3_to_1("Sec") == "U"
+    assert amino_acid_code_3_to_1("Pyl") == "O"
+    assert amino_acid_code_3_to_1("Foo") is None
 
 
 # python -m src.biomolecular_tools.amino_acids
